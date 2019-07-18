c789cfe7ffc9802a901e8a4c8ee2cae1427f0b20
yasha2yashaa <yasha2yashaa@gmail.com>
2019-07-19 01:38:27 +0200

    Refactored FatherService to use IService interface

--- src/main/java/com/codecool/springbootrestapi/services/FatherService.java
+++ src/main/java/com/codecool/springbootrestapi/services/FatherService.java
@@ -9,36 +9,42 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 @Service
-public class FatherService {
+public class FatherService implements IService<Father> {
     private FatherRepository fatherRepository;
 
     public FatherService(@Autowired FatherRepository fatherRepository) {
         this.fatherRepository = fatherRepository;
     }
 
+    @Override
     public void add(Father father) {
         fatherRepository.save(father);
     }
 
+    @Override
     public Father getById(long id) {
         return fatherRepository.findById(id).get();
     }
 
-    public Collection<Father> getAllFathers() {
+    @Override
+    public Collection<Father> getAll() {
         Collection<Father> fathers = new ArrayList<>();
         fatherRepository.findAll().forEach(fathers::add);
         return fathers;
     }
 
+    @Override
     public void update(Father father) {
         if(fatherRepository.existsById(father.getId()))
             fatherRepository.save(father);
     }
 
+    @Override
     public void deleteById(long id) {
         fatherRepository.deleteById(id);
     }
 
+    @Override
     public void deleteAll() {
         fatherRepository.deleteAll();
     }
