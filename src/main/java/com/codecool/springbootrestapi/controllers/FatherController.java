46b2460c42b1dd3f9b8f50a9c25d59074ee39db5
yasha2yashaa <yasha2yashaa@gmail.com>
2019-07-18 16:05:06 +0200

    Finished FatherController class

--- src/main/java/com/codecool/springbootrestapi/controllers/FatherController.java
+++ src/main/java/com/codecool/springbootrestapi/controllers/FatherController.java
@@ -1,16 +1,44 @@
 package com.codecool.springbootrestapi.controllers;
 
+import com.codecool.springbootrestapi.models.Father;
 import com.codecool.springbootrestapi.services.FatherService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/fathers")
 public class FatherController {
-    FatherService fatherService;
+    private FatherService fatherService;
 
     public FatherController(@Autowired FatherService fatherService) {
         this.fatherService = fatherService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Collection<Father> getAll() {
+        return fatherService.getAllFathers();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Father getById(@PathVariable("id") long id) {
+        return fatherService.getById(id);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.OPTIONS)
+    public void deleteById(@PathVariable("id") long id) {
+        fatherService.deleteById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void add(@RequestBody Father father) {
+        fatherService.add(father);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void update(@RequestBody Father father) {
+        fatherService.update(father);
     }
 }
