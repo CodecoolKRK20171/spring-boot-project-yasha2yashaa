8550a96c84924293206592a7c07c5daab204c5d6
yasha2yashaa <yasha2yashaa@gmail.com>
2019-07-19 01:28:39 +0200

    Changed SonRepository class to interface and finished it

--- src/main/java/com/codecool/springbootrestapi/repositories/SonRepository.java
+++ src/main/java/com/codecool/springbootrestapi/repositories/SonRepository.java
@@ -1,4 +1,9 @@
 package com.codecool.springbootrestapi.repositories;
 
-public class SonRepository {
+import com.codecool.springbootrestapi.models.Son;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SonRepository extends CrudRepository<Son, Long> {
 }
