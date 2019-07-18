4c191623f5f69eccc4833b0d9517996a157ed369
yasha2yashaa <yasha2yashaa@gmail.com>
2019-07-18 15:26:32 +0200

    Added annotation to FatherRepository

--- src/main/java/com/codecool/springbootrestapi/repositories/FatherRepository.java
+++ src/main/java/com/codecool/springbootrestapi/repositories/FatherRepository.java
@@ -4,6 +4,8 @@ import com.codecool.springbootrestapi.models.Father;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+// FatherRepository will be created by Spring, because of extending CrudRepository interface
+
 @Repository
 public interface FatherRepository extends CrudRepository<Father, Long> {
 }
