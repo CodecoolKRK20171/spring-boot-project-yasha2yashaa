94f034b0b26256cc1c400fb18b42bc636d5861a2
yasha2yashaa <yasha2yashaa@gmail.com>
2019-07-19 09:35:04 +0200

    Finished SonService

--- src/main/java/com/codecool/springbootrestapi/services/SonService.java
+++ src/main/java/com/codecool/springbootrestapi/services/SonService.java
@@ -1,37 +1,50 @@
 package com.codecool.springbootrestapi.services;
 
 import com.codecool.springbootrestapi.models.Son;
+import com.codecool.springbootrestapi.repositories.SonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class SonService implements IService<Son>{
-    @Override
-    public void add(Son model) {
 
+    private SonRepository sonRepository;
+
+    public SonService(SonRepository sonRepository) {
+        this.sonRepository = sonRepository;
+    }
+
+    @Override
+    public void add(Son son) {
+        sonRepository.save(son);
     }
 
     @Override
     public Son getById(long id) {
-        return null;
+        return sonRepository.findById(id).get();
     }
 
     @Override
     public Collection<Son> getAll() {
-        return null;
+        Collection<Son> sons = new ArrayList<>();
+        sonRepository.findAll().forEach(sons::add);
+        return sons;
     }
 
     @Override
-    public void update(Son entity) {
-
+    public void update(Son son) {
+        if(sonRepository.existsById(son.getId()))
+            sonRepository.save(son);
     }
 
     @Override
     public void deleteById(long id) {
-
+        sonRepository.deleteById(id);
     }
 
     @Override
     public void deleteAll() {
-
+        sonRepository.deleteAll();
     }
 }
