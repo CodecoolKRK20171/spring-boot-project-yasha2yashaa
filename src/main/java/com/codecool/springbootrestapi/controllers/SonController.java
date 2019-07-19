c0f96c9073b12b9a7735358ff2915615ecc9a35e
yasha2yashaa <yasha2yashaa@gmail.com>
2019-07-19 09:26:22 +0200

    Added SonController

--- src/main/java/com/codecool/springbootrestapi/controllers/SonController.java
+++ src/main/java/com/codecool/springbootrestapi/controllers/SonController.java
@@ -1,4 +1,40 @@
 package com.codecool.springbootrestapi.controllers;
 
-public class SonController {
+import com.codecool.springbootrestapi.models.Father;
+import com.codecool.springbootrestapi.models.Son;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+
+@RequestMapping
+public class SonController implements IController<Son> {
+    @Override
+    public Collection<Father> getAll() {
+        return null;
+    }
+
+    @Override
+    public Father getById(long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(long id) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public void add(Son entity) {
+
+    }
+
+    @Override
+    public void update(Son entity) {
+
+    }
 }
