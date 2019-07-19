b3761354635c494ce62b22afc4a8e7e6271f7b3f
yasha2yashaa <yasha2yashaa@gmail.com>
2019-07-19 09:56:23 +0200

    Finished SonController

--- src/main/java/com/codecool/springbootrestapi/controllers/SonController.java
+++ src/main/java/com/codecool/springbootrestapi/controllers/SonController.java
@@ -2,39 +2,49 @@ package com.codecool.springbootrestapi.controllers;
 
 import com.codecool.springbootrestapi.models.Father;
 import com.codecool.springbootrestapi.models.Son;
+import com.codecool.springbootrestapi.services.SonService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 
-@RequestMapping
+@RestController
+@RequestMapping("/sons")
 public class SonController implements IController<Son> {
+
+    private SonService sonService;
+
+    public SonController(@Autowired SonService sonService) {
+        this.sonService = sonService;
+    }
     @Override
-    public Collection<Father> getAll() {
-        return null;
+    public Collection<Son> getAll() {
+        return sonService.getAll();
     }
 
     @Override
-    public Father getById(long id) {
-        return null;
+    public Son getById(long id) {
+        return sonService.getById(id);
     }
 
     @Override
     public void deleteById(long id) {
-
+        sonService.deleteById(id);
     }
 
     @Override
     public void deleteAll() {
-
+        sonService.deleteAll();
     }
 
     @Override
-    public void add(Son entity) {
-
+    public void add(Son son) {
+        sonService.add(son);
     }
 
     @Override
-    public void update(Son entity) {
-
+    public void update(Son son) {
+        sonService.update(son);
     }
 }
