aad22a18d8291804e2f011256098ce0e81a5240b
yasha2yashaa <yasha2yashaa@gmail.com>
2019-07-19 02:02:08 +0200

    Refactored FatherController to use IController interface

--- src/main/java/com/codecool/springbootrestapi/controllers/FatherController.java
+++ src/main/java/com/codecool/springbootrestapi/controllers/FatherController.java
@@ -10,38 +10,44 @@ import java.util.Collection;
 
 @RestController
 @RequestMapping("/fathers")
-public class FatherController {
+public class FatherController implements IController<Father> {
     private FatherService fatherService;
 
     public FatherController(@Autowired FatherService fatherService) {
         this.fatherService = fatherService;
     }
 
+    @Override
     @RequestMapping(method = RequestMethod.GET)
     public Collection<Father> getAll() {
         return fatherService.getAll();
     }
 
+    @Override
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public Father getById(@PathVariable("id") long id) {
         return fatherService.getById(id);
     }
 
+    @Override
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public void deleteById(@PathVariable("id") long id) {
         fatherService.deleteById(id);
     }
 
+    @Override
     @RequestMapping(method = RequestMethod.DELETE)
     public void deleteAll() {
         fatherService.deleteAll();
     }
 
+    @Override
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     public void add(@RequestBody Father father) {
         fatherService.add(father);
     }
 
+    @Override
     @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
     public void update(@RequestBody Father father) {
         fatherService.update(father);
