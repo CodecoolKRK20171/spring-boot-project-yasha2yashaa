1205d4cbf46af41085a8d19b4d850676100acb63
yasha2yashaa <yasha2yashaa@gmail.com>
2019-07-19 09:26:34 +0200

    Added SonService

---
+++ src/main/java/com/codecool/springbootrestapi/services/SonService.java
@@ -0,0 +1,37 @@
+package com.codecool.springbootrestapi.services;
+
+import com.codecool.springbootrestapi.models.Son;
+
+import java.util.Collection;
+
+public class SonService implements IService<Son>{
+    @Override
+    public void add(Son model) {
+
+    }
+
+    @Override
+    public Son getById(long id) {
+        return null;
+    }
+
+    @Override
+    public Collection<Son> getAll() {
+        return null;
+    }
+
+    @Override
+    public void update(Son entity) {
+
+    }
+
+    @Override
+    public void deleteById(long id) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+}
